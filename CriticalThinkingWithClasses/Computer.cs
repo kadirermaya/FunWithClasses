b5485f224a196a86d693173e53c4247cf754cc7d
kadirermaya <kadir.ermaya@gmail.com>
2020-06-10 22:08:47 -0700

    Computer Created

--- CriticalThinkingWithClasses/Computer.cs
+++ CriticalThinkingWithClasses/Computer.cs
@@ -6,10 +6,44 @@ using System.Threading.Tasks;
 
 namespace CriticalThinkingWithClasses
 {
-    class Computer
+    public class Computer
     {
         // MEMBER VARIABLES ( HAS A)
+
+        public Motherboard motherboard;
+        public CPU cpu;
+        public GPU gpu;
+        public HardDrive hardDrive;
+        public RAM ram;
+
         // CONSTRUCTOR (SPAWNER)
+
+        public Computer()
+        {
+            hardDrive = new HardDrive(1024, 1024);
+            ram = new RAM(32, "Kingston");
+            gpu = new GPU("Nvidia", 8.0);
+            cpu = new CPU("Intel", "i9");
+            motherboard = new Motherboard("MSI Gaming", cpu, ram, hardDrive, gpu);
+        }
+
         //MEMBER METHODS (CAN DO)
+
+        public void Run()
+        {
+            Console.WriteLine("Your computer created!");
+            Console.WriteLine($"Your computer's specs:");
+            Console.WriteLine($"Processor:      {cpu.manufacturer} {cpu.name}");
+            Console.WriteLine($"Graphics Card:  {gpu.Manufacturer} {gpu.EffectiveMemory} gb");
+            Console.WriteLine($"Ram:            {ram.Brand} {ram.TotalGigabytes}");
+            Console.WriteLine($"Hard Drive:     {hardDrive.Storage} gb");
+            Console.WriteLine($"Motherboard:    {motherboard.Manufacturer}");
+        }
+
+        public void AddApplication()
+        {
+            
+        }
+        
     }
 }
